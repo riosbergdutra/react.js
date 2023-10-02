ae3298ef979f0d1c11b9262c493678dcb8c4892c
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-10-02 13:57:42 -0300

    aprendendo a acessar propriedades de um componente

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Welcome from './components/welcome';
 
 function App() {
   return (
     <div className="App">
       <h1>Olá</h1>
+      <Welcome nome = "ivo" sobrenome = "soares"/>
+      <Welcome nome = "maria"/>
+      <Welcome nome = "joão"/>
     </div>
   );
 }
---
+++ my-app/src/components/welcome.js
@@ -0,0 +1,6 @@
+function Welcome (props) {
+    return (
+        <h1>Olá, {props.nome} {props.sobrenome} seja bem vindo !</h1>
+    )
+}
+export default Welcome

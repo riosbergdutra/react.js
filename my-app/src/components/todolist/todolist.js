2b668457fe0951263cb7f2a637f4f3e089ad44e9
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-10-25 15:31:56 -0300

    Trabalhando com Hook useState

--- my-app/src/components/todolist/todolist.js
+++ my-app/src/components/todolist/todolist.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './todolist.css';
 import Todoitem from '../todoitem/todoitem';
 
 const Todolist = () => {
-    const tarefas = ['ir ao mercado', 'estudar javascript','estudar react', 'lavar a louça', 'novo item'];
-
+    const [tarefas, setTarefas] = useState(['ir ao supermercado'])
+    //const [contador, setContador] = useState(0)
+    //const tarefas = ['ir ao mercado', 'estudar javascript','estudar react', 'lavar a louça', 'novo item'];
+    const handleclick = () => {
+        console.log('clicou')
+      setTarefas([...tarefas,'cortar o cabelo'])
+    }
     return (
         <section className="todolist">
             <h2 className='todo-title'>Minha Lista de Tarefas</h2>
@@ -12,6 +18,7 @@ const Todolist = () => {
             <Todoitem tarefa={tarefa} key={tarefa} />
             ))}
             </ol>
+            <button onClick={handleclick}>adicionar tarefas</button>
         </section>
     );
 }

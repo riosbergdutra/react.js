f86f1cd3ae6409b80baede5a4812e5a696b68a06
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-11-13 17:05:40 -0300

    utilizando styled-components

--- marketplace/src/components/navmenu/index.js
+++ marketplace/src/components/navmenu/index.js
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom"
 import './style.css'
+import styled from "styled-components"
 const Navmenu = () => {
+    const Button = styled.button`
+        background: palevioletred;
+        border-radius: 3px;
+        border: 2px solid palevioletred;
+        color: white;
+        margin: 0 1em;
+        padding: 0.25em 1em;
+    `;
+
+
     return (
         <nav>
             <h1>MarketPlace</h1>
             <ul>
                 <li>
                     <Link to='/'>Home</Link>
-                </li>
+                </li> 
                 <li>
                     <Link to='/login'>Login</Link>
                     
@@ -17,6 +28,9 @@ const Navmenu = () => {
                 </li>
                 <li>
                 <Link to='/register'>Register</Link>
+                </li>
+                <li>
+                    <Button>clique aqui</Button>
                 </li>
             </ul>
         </nav>    

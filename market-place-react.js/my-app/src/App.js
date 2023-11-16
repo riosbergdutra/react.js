9737589650bf1df53966609d0bf6fc81c1615d8b
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-11-16 15:05:07 -0300

    Introdução ao Twindcss

---
+++ market-place-react.js/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Olá Mundo</h1>
+      <p>Bem vindos</p>
+    </>
+  );
+}
+
+export default App;

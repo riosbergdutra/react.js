710a2d11247137fbfcaaee2111cefebe7e3e183e
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-11-18 15:28:04 -0300

    feat: css na home

--- market-place-react.js/my-app/src/pages/Home/index.js
+++ market-place-react.js/my-app/src/pages/Home/index.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className='home-banner w-full'>
+      <div className='flex flex-col items-center justify-center h-full'>
+    <h1 className='text-center text-3xl md:text-4xl lg:text-5xl font-semibold text-gray-700'>Os melhores alimentos você encontra aqui</h1>
+    </div>
+    </section>
+    </>
   )
 }
 

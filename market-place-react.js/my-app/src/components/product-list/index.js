4a7d69b17c2636cbceb53768a40cd6c6a80e0f02
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-11-19 14:39:23 -0300

    imagem para hamburger

--- market-place-react.js/my-app/src/components/product-list/index.js
+++ market-place-react.js/my-app/src/components/product-list/index.js
@@ -1,22 +1,57 @@
-import React, { useState } from 'react'
-import Product from '../product'
+import React, { useState } from 'react';
+import Product from '../product';
 
 const Productlist = () => {
-    const [categoriaTab, setcategoriatab] = useState('Hamburger')
+  const [categoriaTab, setCategoriaTab] = useState('Hamburger');
+
   return (
-    <section className='my-12 max-w-screen-xl mx-auto px-3'>
+    <>
+      <section className='my-12 max-w-screen-xl mx-auto px-3'>
         {/* {menu de categoria} */}
         <div className='flex items-center justify-center space-x-6'>
-            <p className='active-menu-tab bg-primary'>Hamburger</p>
-            <p className='menu-tab'>Brasileira</p>
-            <p className='menu-tab'>Japonesa</p>
+          <p
+            className={
+              categoriaTab === 'Hamburger'
+                ? 'active-menu-tab bg-primary'
+                : 'menu-tab'
+            }
+            onClick={() => setCategoriaTab('Hamburger')}
+          >
+            Hamburger
+          </p>
+          <p
+            className={
+              categoriaTab === 'Brasileira'
+                ? 'active-menu-tab bg-primary'
+                : 'menu-tab'
+            }
+            onClick={() => setCategoriaTab('Brasileira')}
+          >
+            Brasileira
+          </p>
+          <p
+            className={
+              categoriaTab === 'Japonesa'
+                ? 'active-menu-tab bg-primary'
+                : 'menu-tab'
+            }
+            onClick={() => setCategoriaTab('Japonesa')}
+          >
+            Japonesa
+          </p>
         </div>
         {/* lista de produtos */}
-        <div className=''>
-            <Product/>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-12'>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
         </div>
-    </section>
-  )
-}
+      </section>
+    </>
+  );
+};
 
-export default Productlist
+export default Productlist;
--- market-place-react.js/my-app/src/components/product/index.js
+++ market-place-react.js/my-app/src/components/product/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import  hamburger from '../../assets/hamburger.jpg'
 const Product = () => {
   return (
-    <div>product</div>
-  )
-}
+    <div className='bg-white border border-gray-100 transition transform duration-700 hover:shadow-xl hover:scale-105 p-4 rounded-lg relative'>
+      <span className='bg-red-100 border border-red-500 rounded-full text-primary text-sm px-4 py-1 inline-block mb-4'>Hamburger</span>
+      <img className='w-64 mx-auto transform transition duration-300 hover:scale-105' src={hamburger} alt='imagem do produto'/>
+      <div className='flex flex-col items-center my-3 space-y-2'>
+        <h1 className='text-gray-900 text-lg'>Hamburger suculento</h1>
+        <p className='text-gray-500 text-sm text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit</p>
+        <h2 className='text-gray-900 text-2xl font-bold'>$25,00</h2>
+        <button className='bg-primary text-white px-8 py-2 rounded-full transition transform duration-300 hover:shadow-xl hover:scale-105'>Pedir agora</button>
+      </div>
+    </div>
+  );
+};
 
-export default Product
+export default Product;

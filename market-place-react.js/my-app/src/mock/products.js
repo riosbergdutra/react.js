4c28f93ebca268aad1b0a5c86290594d1059bb07
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-11-20 17:01:18 -0300

    inicio de dados dos produtos

---
+++ market-place-react.js/my-app/src/mock/products.js
@@ -0,0 +1,27 @@
+const productsMock = [
+    {
+        _id: "1",
+        nome: "X-Burger",
+        descricao: "X-Burger bem suculento com molho especial",
+        precoUnitario: 10,
+        image: "https://img.freepik.com/fotos-gratis/hamburguer-isolado-no-fundo-branco-fastfood-de-hamburguer-fresco-com-carne-e-queijo_90220-1329.jpg?size=338&ext=jpg&ga=GA1.1.1880011253.1699747200&semt=ais",
+        codigobarra: 111111
+    },
+    {
+        _id: "2",
+        nome: "Hamburger",
+        descricao: "Hamburger bem suculento com molho especial",
+        precoUnitario: 8,
+        image: "https://img.freepik.com/fotos-gratis/hamburguer-isolado-no-fundo-branco-fastfood-de-hamburguer-fresco-com-carne-e-queijo_90220-1329.jpg?size=338&ext=jpg&ga=GA1.1.1880011253.1699747200&semt=ais",
+        codigobarra: 196789
+    },
+    {
+        _id: "3",
+        nome: "Coca cola",
+        descricao: "Coca cola gelada",
+        precoUnitario: 5,
+        image: "https://ribeirao.emporiotartufo.com.br/image/cache/catalog/produtos/Coca-Cola/116333-coca-cola-lata-350ml-1-1000x667.png",
+        codigobarra: 196398
+    }
+]
+export default productsMock;

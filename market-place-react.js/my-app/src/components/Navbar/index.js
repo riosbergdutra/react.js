84cec314fcd999b2f962f1e485c09891a78edaf6
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-11-21 15:45:12 -0300

    Feat: link para login e home

--- market-place-react.js/my-app/src/components/Navbar/index.js
+++ market-place-react.js/my-app/src/components/Navbar/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-      <header className='bg-transparent z-50 w-full'>
-        <nav className='flex items-center max-w-screen-xl mx-auto px-6 py-3'>
-            <div className='flex flex-grow items-center'>
+    <header className='bg-transparent z-50 w-full'>
+      <nav className='flex items-center max-w-screen-xl mx-auto px-6 py-3'>
+        <div className='flex flex-grow items-center'>
           <img src={logo} alt='logo' className='w-36 cursor-pointer'/>
-          <h1 className='text-center text-3xl font-semibold text-gray-700'>Food App</h1>
-          </div>
-          <div className='flex items-center justify-end space-x-6'>
-          <button>Login</button>
+          <h1 className='text-center text-3xl font-semibold text-gray-700'><Link to='/'>Food App</Link></h1>
+        </div>
+        <div className='flex items-center justify-end space-x-6'>
+          <button><Link to='/login'>Login</Link></button>
           <button className='bg-primary px-6 py-3 text-white rounded-full transition duration-700 hover:scale-105'>Register</button>
-          </div>
-        </nav>
-      </header>
-   
+        </div>
+      </nav>
+    </header>
   );
 }
 

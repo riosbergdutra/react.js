45bfe635cd2324ab54a2d29c757177629418eea6
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-11-22 14:02:21 -0300

    Manipulação dos formulários

--- market-place-react.js/my-app/src/pages/login/index.js
+++ market-place-react.js/my-app/src/pages/login/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [inputValues, setInputValues] = useState({
+    email: '',
+    senha: ''
+})
+
+const navigate = useNavigate()
+
+  const handleChangeValues = (evento) => {
+    console.log(evento.target.name)
+    console.log(evento.target.value)
+    setInputValues({
+      ...inputValues,
+      [evento.target.name]: evento.target.value
+    })
+    console.log(inputValues)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(`usuario ${inputValues.email} logado com sucesso`)
+    navigate('/')
+  }
+
   return (
     <main className='h-screen w-full banner'>
       <div className='flex flex-col items-center pt-20 h-screen'>
         <img className='w-52' src={logo} alt='logo-tipo food app' />
-        <form
+        <form onSubmit={handleSubmit}
           action=''
           className='bg-white w-96 mt-6 p-4 rounded-lg shadow-lg'
         >
@@ -16,15 +40,17 @@ const Login = () => {
               placeholder='Digite seu email:'
               name='email'
               className='w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300'
+              onChange={handleChangeValues}
             />
             <input
               type='password'
               placeholder='Digite sua senha:'
               name='senha'
               className='w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300'
+              onChange={handleChangeValues}
             />
           </div>
-          <button className='w-full py-3 bg-primary text-white focus:outline-none focus:ring-4 mt-6 rounded-lg transition duration-300'>
+          <button type='submit' className='w-full py-3 bg-primary text-white focus:outline-none focus:ring-4 mt-6 rounded-lg transition duration-300'>
             Entrar
           </button>
           <p className='text-base text-primary text-center my-6 hover:underline cursor-pointer'> precisa de uma conta ?</p>

ed2ce89c76f288fedf7993c8653802308b96ca4d
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-11-26 12:19:22 -0300

    Feat: POST do front-end para back-end

--- market-place-react.js/my-app/src/pages/login/index.js
+++ market-place-react.js/my-app/src/pages/login/index.js
@@ -6,13 +6,11 @@ const Login = () => {
   const [inputValues, setInputValues] = useState({
     email: '',
     senha: ''
-})
+  });
 
-const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChangeValues = (evento) => {
-    console.log(evento.target.name)
-    console.log(evento.target.value)
     setInputValues({
       ...inputValues,
       [evento.target.name]: evento.target.value
@@ -20,24 +18,25 @@ const navigate = useNavigate()
     console.log(inputValues)
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-   /* futuramente vai colocar uma fetch com login const response = await fetch('http://localhost:3000/auth/login {
-    method: 'POST',
-    headers: {
-     'content:type': 'aplication/json
-    },
-    body: JSON.stringfy(inputValues)
-    //})*/
-    alert(`usuario ${inputValues.email} logado com sucesso`)
-    navigate('/')
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+   const response = await fetch('http://localhost:5000/usuario/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputValues)
+    })
+    console.log(response)
+      navigate('/');
   }
 
   return (
     <main className='h-screen w-full banner'>
       <div className='flex flex-col items-center pt-20 h-screen'>
         <img className='w-52' src={logo} alt='logo-tipo food app' />
-        <form onSubmit={handleSubmit}
+        <form
+          onSubmit={handleSubmit}
           action=''
           className='bg-white w-96 mt-6 p-4 rounded-lg shadow-lg'
         >
@@ -57,14 +56,18 @@ const navigate = useNavigate()
               onChange={handleChangeValues}
             />
           </div>
-          <button type='submit' className='w-full py-3 bg-primary text-white focus:outline-none focus:ring-4 mt-6 rounded-lg transition duration-300'>
+          <button
+            type='submit'
+            className='w-full py-3 bg-primary text-white focus:outline-none focus:ring-4 mt-6 rounded-lg transition duration-300'
+          >
             Entrar
           </button>
-          <p className='text-base text-primary text-center my-6 hover:underline cursor-pointer'> precisa de uma conta ?</p>
+          <p className='text-base text-primary text-center my-6 hover:underline cursor-pointer'>
+            precisa de uma conta ?
+          </p>
         </form>
       </div>
     </main>
   );
 };
-
-export default Login;
+export default Login

6ca1cd93e469c812c7995a768d160df563fdf762
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-08 15:00:16 -0300

    Feat: mostrando nome e imagem do usuario

--- market-place-react.js/my-app/src/components/Navbar/index.js
+++ market-place-react.js/my-app/src/components/Navbar/index.js
@@ -1,43 +1,53 @@
 import React, { useContext } from 'react';
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
-import { BsFillCartFill } from "react-icons/bs";
-import { MdLogout } from "react-icons/md";
+import { BsFillCartFill } from 'react-icons/bs';
+import { MdLogout } from 'react-icons/md';
 
 const Navbar = () => {
-  const {userLogged, logout} = useContext(AuthContext);
-console.log(`valor do contexto`, userLogged);
-  const navigate = useNavigate()
-
+  const { userLogged, logout, userData } = useContext(AuthContext);
+  console.log(`valor do contexto`, userLogged);
+  const navigate = useNavigate();
+ 
+  
   return (
     <header className='bg-transparent z-50 w-full'>
       <nav className='flex items-center max-w-screen-xl mx-auto px-6 py-3'>
         <div className='flex flex-grow items-center'>
-          <img src={logo} alt='logo' className='w-36 cursor-pointer'/>
-          <h1 className='text-center text-3xl font-semibold text-gray-700'><Link to='/'>Food App</Link></h1>
+          <img src={logo} alt='logo' className='w-36 cursor-pointer' />
+          <h1 className='text-center text-3xl font-semibold text-gray-700'>
+            <Link to='/'>Food App</Link>
+          </h1>
         </div>
-        {
-          userLogged ? (
-            <div className=' flex items-center justify-end space-x-4'>
-              <div className='relative flex cursor-pointer'>
+        {userLogged ? (
+          <div className=' flex items-center justify-end space-x-4'>
+            <div className='relative flex cursor-pointer'>
               <span className='bg-primary w-4 h-4 rounded-full p-1 flex items-center justify-center text-white absolute -right-2 -top-2'></span>
               <BsFillCartFill className='w-6 h-6 cursor-pointer' />
-              </div>  
-                <img src='' alt=''/>
-                <p className='text-gray-700'> Bem Vindo, Nome do usuario</p>
-                <MdLogout className='w-6 h-6 cursor-pointer' onClick={logout}/>
-               </div>
-          ) : (
-            <div className='flex items-center justify-end space-x-6'>
-          <button onClick={()=>navigate('/login')}>Login</button>
-          <button onClick={()=>navigate('/register')} className='bg-primary px-6 py-3 text-white rounded-full transition duration-700 hover:scale-105'>Register</button>
-        </div>
-          )
-        }
+            </div>
+            <p className='text-gray-700'>
+               Bem Vindo, {userData.nome}!
+            </p>
+              {userData && userData.imagem && (
+                <img src={userData.imagem} alt='imagem do usuario' className='w-10 h-10 rounded-full' />
+              )}
+            <MdLogout className='w-6 h-6 cursor-pointer' onClick={logout} />
+          </div>
+        ) : (
+          <div className='flex items-center justify-end space-x-6'>
+            <button onClick={() => navigate('/login')}>Login</button>
+            <button
+              onClick={() => navigate('/register')}
+              className='bg-primary px-6 py-3 text-white rounded-full transition duration-700 hover:scale-105'
+            >
+              Register
+            </button>
+          </div>
+        )}
       </nav>
     </header>
   );
-}
+};
 
 export default Navbar;

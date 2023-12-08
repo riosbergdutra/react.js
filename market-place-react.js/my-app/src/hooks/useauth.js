c636c006a3e0d8b0bd59dc06a0a336f6120a415a
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-08 15:02:43 -0300

    Feat: atualizando a pagina com nome e imagem

--- market-place-react.js/my-app/src/hooks/useauth.js
+++ market-place-react.js/my-app/src/hooks/useauth.js
@@ -1,51 +1,72 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { loginUserApi } from "../services/authservice";
+import { loginUserApi, loginUserId } from "../services/authservice";
 
 const UseAuth = () => {
-    // Estado para controlar se o usuário está logado ou não
-    const [userLogged, setUserLogged] = useState(false);
-    const [loading, setLoading] = useState(true);
+  const [userLogged, setUserLogged] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
 
-    // Hook do React Router para navegação
-    const navigate = useNavigate();
+  // Hook do React Router para navegação
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        // Tente obter os dados do usuário do localStorage ao iniciar a aplicação
-        const storedUserInfo = localStorage.getItem('userinfo');
+  useEffect(() => {
+    // Tente obter os dados do usuário do localStorage ao iniciar a aplicação
+    const storedUserInfo = localStorage.getItem('userinfo');
 
-        if (storedUserInfo) {
-            // Se houver dados no localStorage, atualize o estado do usuário como logado
-            setUserLogged(true);
+    console.log("Dados do localStorage:", storedUserInfo);
+
+    if (storedUserInfo) {
+      setUserData(JSON.parse(storedUserInfo));
+      setUserLogged(true);
+    }
+
+    setLoading(false);
+  }, []); // Este efeito só é executado uma vez ao montar o componente
+
+  // Função para realizar o login do usuário
+  const loginUser = async (inputValues) => {
+    try {
+      const data = await loginUserApi(inputValues);
+  
+      if (data && data._id) {
+        console.log('Dados do usuário após login:', data);
+  
+        const response = await loginUserId(data._id);
+  
+        console.log('Resposta da API de loginUserId:', response);
+  
+        // Verifique se a resposta contém a propriedade 'nome'
+        if (response && response.nome) {
+          // Certifique-se de incluir a propriedade 'imagem' ao atualizar o estado
+          const updatedUserData = { ...data, ...response, nome: response.nome, imagem: response.imagem || '' };
+  
+          localStorage.setItem('userinfo', JSON.stringify({ data, response, nome: response.nome, imagem: response.imagem || '' }));
+  
+          setUserData(updatedUserData);
+          setUserLogged(true);
+          navigate('/');
+        } else {
+          console.error('Nome do usuário não encontrado nos dados da resposta:', response);
         }
-        setLoading(false);
-    }, []); // Este efeito só é executado uma vez ao montar o componente
+      } else {
+        console.error('ID não encontrado nos dados do usuário:', data);
+      }
+    } catch (error) {
+      console.error('Erro durante o login:', error);
+      // Trate o erro conforme necessário
+    }
+  };
 
-    // Função para realizar o login do usuário
-    const loginUser = async (inputValues) => {
-        try {
-            const data = await loginUserApi(inputValues);
+  // Função para realizar o logout do usuário
+  const logout = () => {
+    setUserLogged(false);
+    localStorage.clear();
+    navigate('/login');
+  };
 
-            // Salve os dados do usuário no localStorage
-            localStorage.setItem('userinfo', JSON.stringify(data));
-
-            // Atualize o estado do usuário como logado e redirecione para a página inicial
-            setUserLogged(true);
-            navigate('/');
-        } catch (error) {
-            console.error('Erro durante o login:', error);
-            // Trate o erro conforme necessário (ex.: exibindo uma mensagem de erro para o usuário)
-        }
-    };
-
-    // Função para realizar o logout do usuário
-    const logout = () => {
-        setUserLogged(false);
-        localStorage.clear();
-        navigate('/login');
-    };
-
-    return { userLogged, loading, loginUser, logout };
+  return { userLogged, loading, loginUser, logout, userData };
 }
 
 export default UseAuth;
+

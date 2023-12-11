24dc3a82877c0a831273151d15966e613087b789
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-11 13:32:34 -0300

    marcando savepoint para depois

--- market-place-react.js/my-app/src/hooks/useauth.js
+++ market-place-react.js/my-app/src/hooks/useauth.js
@@ -11,7 +11,6 @@ const UseAuth = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Tente obter os dados do usuário do localStorage ao iniciar a aplicação
     const storedUserInfo = localStorage.getItem('userinfo');
 
     console.log("Dados do localStorage:", storedUserInfo);
@@ -22,43 +21,35 @@ const UseAuth = () => {
     }
 
     setLoading(false);
-  }, []); // Este efeito só é executado uma vez ao montar o componente
+  }, []); 
 
   // Função para realizar o login do usuário
   const loginUser = async (inputValues) => {
     try {
       const data = await loginUserApi(inputValues);
   
-      if (data && data._id) {
+      if (data && data.email && data.senha) {
         console.log('Dados do usuário após login:', data);
   
-        const response = await loginUserId(data._id);
+        const updatedUserData = {
+          ...data,
+          nome: data.nome || '',
+          imagem: data.imagem || ''
+        };
   
-        console.log('Resposta da API de loginUserId:', response);
-  
-        // Verifique se a resposta contém a propriedade 'nome'
-        if (response && response.nome) {
-          // Certifique-se de incluir a propriedade 'imagem' ao atualizar o estado
-          const updatedUserData = { ...data, ...response, nome: response.nome, imagem: response.imagem || '' };
-  
-          localStorage.setItem('userinfo', JSON.stringify({ data, response, nome: response.nome, imagem: response.imagem || '' }));
-  
-          setUserData(updatedUserData);
-          setUserLogged(true);
-          navigate('/');
-        } else {
-          console.error('Nome do usuário não encontrado nos dados da resposta:', response);
-        }
+        localStorage.setItem('userinfo', JSON.stringify({ id: data._id, nome: data.nome || '', imagem: data.imagem || '' }));
+        setUserData(updatedUserData);
+        setUserLogged(true);
+        navigate('/');
       } else {
-        console.error('ID não encontrado nos dados do usuário:', data);
+        console.error('ID do usuário não encontrado nos dados:', data);
+        // Se o ID não está diretamente em data._id, ajuste de acordo com a estrutura da resposta da sua API
       }
     } catch (error) {
       console.error('Erro durante o login:', error);
       // Trate o erro conforme necessário
     }
   };
-
-  // Função para realizar o logout do usuário
   const logout = () => {
     setUserLogged(false);
     localStorage.clear();

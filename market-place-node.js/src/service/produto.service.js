7aac62bf60bad5bcf5bebd2432c00102268b8121
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-15 15:46:28 -0300

    Feat: service para produto

---
+++ market-place-node.js/src/service/produto.service.js
@@ -0,0 +1,51 @@
+const Produto = require('../model/produto');
+
+const listAllProdutos = async () => {
+  try {
+    return await Produto.find();
+  } catch (err) {
+    throw err;
+  }
+}
+
+const createProduto = async (produtoData) => {
+  try {
+    const novaCategoria = new Produto(produtoData);
+    return await novaCategoria.save();
+} catch (err) {
+    throw err;
+}
+};
+
+
+const getProdutoById = async (id) => {
+  try {
+    return await Produto.findById(id);
+  } catch (err) {
+    throw err;
+  }
+}
+
+const updateProduto = async (id, produtoData) => {
+  try {
+    return await Produto.findByIdAndUpdate(id, produtoData, { new: true });
+  } catch (err) {
+    throw err;
+  }
+}
+
+const deleteProduto = async (id) => {
+  try {
+    await Produto.findByIdAndRemove(id);
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = {
+  listAllProdutos,
+  createProduto,
+  getProdutoById,
+  updateProduto,
+  deleteProduto,
+};

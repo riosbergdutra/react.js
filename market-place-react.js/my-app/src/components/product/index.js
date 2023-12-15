9b41e6dc5251b8dabcd8ad052942bf9566e7e9db
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-15 14:25:38 -0300

    Feat: rota para productinfo em product

--- market-place-react.js/my-app/src/components/product/index.js
+++ market-place-react.js/my-app/src/components/product/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 const Product = ({product}) => {
+  const navigate = useNavigate()
   return (
     <div className='bg-white border border-gray-100 transition transform duration-700 hover:shadow-xl hover:scale-105 p-4 rounded-lg relative'>
       <span className='bg-red-100 border border-red-500 rounded-full text-primary text-sm px-4 py-1 inline-block mb-4'>Hamburger</span>
@@ -8,7 +10,7 @@ const Product = ({product}) => {
         <h1 className='text-gray-900 text-lg'>{product.nome}</h1>
         <p className='text-gray-500 text-sm text-center'>{product.descricao}</p>
         <h2 className='text-gray-900 text-2xl font-bold'>R${product.precoUnitario}</h2>
-        <button className='bg-primary text-white px-8 py-2 rounded-full transition transform duration-300 hover:shadow-xl hover:scale-105'>Pedir agora</button>
+        <button onClick={() => navigate(`/product/${product._id}`)} className='bg-primary text-white px-8 py-2 rounded-full transition transform duration-300 hover:shadow-xl hover:scale-105'>Pedir agora</button>
       </div>
     </div>
   );

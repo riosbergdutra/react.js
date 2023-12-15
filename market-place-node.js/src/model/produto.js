a3951112fbc7802774332ba207d6d36f11460562
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-15 15:46:09 -0300

    Feat: model para produto

---
+++ market-place-node.js/src/model/produto.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const produtoSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: true,
+  },
+  descricao: {
+    type: String,
+    required: true,
+  },
+  precoUnitario: {
+    type: Number, 
+    required: true,
+    min: 0,
+  },
+  imagem: {
+    type: String,
+    required: true,
+  },
+  codigoBarra: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  categoria: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Categoria', // Troque 'Categorias' pelo nome correto do modelo de categoria
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: null,
+  },
+});
+
+const Produto = mongoose.model('Produto', produtoSchema);
+
+module.exports = Produto;

8c9827e64ca12de2be876690ddacf943c037e80f
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-16 18:06:07 -0300

    Feat: adicionando novas funcionalidades

--- market-place-react.js/my-app/src/pages/cart/index.js
+++ market-place-react.js/my-app/src/pages/cart/index.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { AiOutlineDelete } from "react-icons/ai";
 
 const Cart = () => {
   const [productCart, setProductCart] = useState([]);
+  const [totalValue, setTotalValue] = useState(0)
 
   useEffect(() => {
     const storageCart = JSON.parse(localStorage.getItem('productCart')) || [];
     setProductCart(storageCart);
+    const total = storageCart.reduce((valor, product) => {
+      return valor + product.precoUnitario
+    }, 0)
+    setTotalValue(total)
   }, []);
 
   return (
@@ -27,7 +33,6 @@ const Cart = () => {
                     id='cep'
                     className='w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300'
                   />
-
                   <input
                     type='text'
                     name='rua'
@@ -56,18 +61,55 @@ const Cart = () => {
           <div className='col-span-1'>
             <div className='grass p-6 box-border rounded-lg'>
               <div className='flex flex-col space-y-3'>
-                {Array.isArray(productCart) &&
+                {productCart &&
                   productCart.map((product) => (
-                    <div key={product._id} className='rounded-lg p-4 space-x-3'>
-                      <div className="flex">
-                        <img className='w-24 object-contain' src={product.imagem} alt={product.nome}/>
-                        </div>
-                        <div className='flex flex-col space-y-3 flex-grow'>
-                            <h5 className='flex-base poppins text-gray-700'>{product.nome}</h5>
-                            <h3 className='font-semibold text-lg text-primary poppins'>R${product.precoUnitario}</h3>
-                        </div>
+                    <div key={product._id} className='rounded-lg p-4 space-x-3 flex'>
+                      <img className='w-24 object-contain' src={product.imagem} alt={product.nome} />
+                      <div className='flex flex-col space-y-3'>
+                        <h5 className='poppins text-gray-1000'>{product.nome}</h5>
+                        <h3 className='font-semibold text-red-600 poppins'>{`R$${product.precoUnitario}`}</h3>
+                      </div>
+                      <div className='flex items-center px-4 py-2 space-x-3'>
+                        <span className='text-lg text-gray-500 select-none'>
+                          {product.quantity}un                 
+                        </span>
+                      </div>
+                      <div className='flex flex-col items-center justify-center'>
+                      <AiOutlineDelete className='w-6 h-6 text-gray-600 transform transition hover:scale-105 duration-500 cursor-pointer'/>
+                      </div>
                     </div>
                   ))}
+              </div>
+              <div className='flex flex-col space-y-3 my-4'>
+                <div className='flex items-center'>
+                  <span className='flex-grow poppins text-gray-700'>
+                    total
+                  </span>
+                  <span className='poppins font-semibold text-black'>
+                    RS{totalValue}
+                  </span>
+                </div>
+                <div className='flex items-center'>
+                  <span className='flex-grow poppins text-gray-700'>
+                    Taxa de entrega
+                  </span>
+                  <span className='poppins font-semibold text-black'>
+                    5
+                  </span>
+                </div>
+                <div className='flex items-center'>
+                  <span className='flex-grow poppins text-gray-700 text-xl'>
+                    Total + taxa:
+                  </span>
+                  <span className='poppins font-semibold text-black text-xl'>
+                    R${totalValue + 5}
+                  </span>
+                </div>
+                <div>
+                <button className='w-full px-6 py-3 bg-primary text-white poppins ring-red-300 focus:ring-4 transition duration-500'>
+                  Enviar Pedido
+                </button>
+                </div>
               </div>
             </div>
           </div>

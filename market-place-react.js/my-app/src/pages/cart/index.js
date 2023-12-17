551acf26c83ad37f4c06ee49a37541b2aaf8f62c
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-17 16:06:06 -0300

    Feat: novas funcoes

--- market-place-react.js/my-app/src/pages/cart/index.js
+++ market-place-react.js/my-app/src/pages/cart/index.js
@@ -1,10 +1,17 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { AiOutlineDelete } from "react-icons/ai";
+import { sendCart } from '../../services/orderService';
 
 const Cart = () => {
   const [productCart, setProductCart] = useState([]);
   const [totalValue, setTotalValue] = useState(0)
-
+  const [adress, setAdress] = useState({
+    cep: '',
+    rua: '',
+    numero: '',
+    complemento: '',
+  })
   useEffect(() => {
     const storageCart = JSON.parse(localStorage.getItem('productCart')) || [];
     setProductCart(storageCart);
@@ -14,8 +21,46 @@ const Cart = () => {
     setTotalValue(total)
   }, []);
 
+  const findAddress = async () => {
+    try {
+      const response = await axios.get(`https://viacep.com.br/ws/${adress.cep}/json`);
+      const data = response.data;
+  
+      setAdress({
+        ...adress,
+        rua: `${data.logradouro}, ${data.bairro}, ${data.localidade}`
+      });
+    } catch (error) {
+      console.error('Erro ao buscar endereço:', error);
+    }
+  };
+
+  const sendOrder = async () => {
+    const productsOrder = productCart.map((product) => {
+      return {
+        _id: product._id,
+        quantidade: product.quantity
+      }
+    })
+    const cartInfo = {
+      produtos: productsOrder,
+      precoTotal: totalValue,
+      frete: 5
+    }
+    const response = await sendCart(cartInfo)
+  }
+
+  const handleChangeValues = (evento) => {
+    setAdress({
+      ...adress,
+      [evento.target.name]: evento.target.value
+    });
+    console.log(adress);
+  };
+
+
   return (
-    <main className='h-screen banner'>
+    <main className='h-screen'>
       <div className='max-w-screen-xl py-20 mx-auto px-6'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10'>
           {/* informações endereço */}
@@ -31,6 +76,8 @@ const Cart = () => {
                     name='cep'
                     placeholder='cep'
                     id='cep'
+                    value={adress.cep}
+                    onChange={handleChangeValues}
                     className='w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300'
                   />
                   <input
@@ -38,6 +85,9 @@ const Cart = () => {
                     name='rua'
                     placeholder='rua'
                     id='rua'
+                    value={adress.rua}
+                    onFocus={findAddress}
+                    onChange={handleChangeValues}
                     className='w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300'
                   />
                   <input
@@ -45,6 +95,8 @@ const Cart = () => {
                     name='numero'
                     placeholder='numero'
                     id='numero'
+                    value={adress.numero}
+                    onChange={handleChangeValues}
                     className='w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300'
                   />
                   <input
@@ -52,6 +104,8 @@ const Cart = () => {
                     name='complemento'
                     placeholder='complemento'
                     id='complemento'
+                    value={adress.complemento}
+                    onChange={handleChangeValues}
                     className='w-full px-4 py-3 rounded-lg ring-red-200 border-gray-300 focus:ring-4 focus:outline-none transition duration-300'
                   />
                 </div>
@@ -105,8 +159,17 @@ const Cart = () => {
                     R${totalValue + 5}
                   </span>
                 </div>
+                <div className='flex flex-col space-y-4 mb-3'>
+                    <p className='poppins text-gray-700'>Endereço de entrega</p>
+                    <span className='font-semibold text-black'>
+                      {adress.rua}
+                    </span>
+                    <span className='font-semibold text-black'>
+                      Numero:{adress.numero} - complemento: {adress.complemento}
+                    </span>
+                </div>
                 <div>
-                <button className='w-full px-6 py-3 bg-primary text-white poppins ring-red-300 focus:ring-4 transition duration-500'>
+                <button onClick={sendOrder} className='w-full px-6 py-3 bg-primary text-white poppins ring-red-300 focus:ring-4 transition duration-500'>
                   Enviar Pedido
                 </button>
                 </div>

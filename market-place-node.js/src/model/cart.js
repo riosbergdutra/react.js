5b46e9483d5287adbaa00cb6027d32e3913e8bc8
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-19 17:47:34 -0300

    Fix: Correcoes do model cart

--- market-place-node.js/src/model/cart.js
+++ market-place-node.js/src/model/cart.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const cartSchema = new mongoose.Schema({
-   produtos: [
-      {
-         produtoId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Produto" },
-         quantidade: { type: Number, required: true }
-      },
-   ],
-   createdAt: { type: Date, required: true, default: Date.now() },
-   precoTotal: { type: Number, required: true },
-   frete: { type: Number, required: true },
-   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Usuario' }
+const CartSchema = new mongoose.Schema({
+    produtos: [
+        {
+            _id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Produto" },
+            quantidade: { type: Number, required: true }
+        },
+    ],
+    createdAt: { type: Date, required: true, default: Date.now() },
+    precoTotal: { type: Number, required: true},
+    frete: { type: Number, required: true},
 });
 
-const Cart = mongoose.model('Cart', cartSchema);
+const Cart = mongoose.model("Cart", CartSchema);
 
 module.exports = Cart;

81489e81e6134f6c58ce4873bc0b35a860b7b64b
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-19 17:51:21 -0300

    Feat: Post para pedido

--- market-place-react.js/my-app/src/services/orderService.js
+++ market-place-react.js/my-app/src/services/orderService.js
@@ -1,9 +1,15 @@
-import api from "./api"
+import api from "./api";
 
 const sendCart = (cartInfo) => {
-    api.post('/cart/create', cartInfo)
+  return api.post('/cart/create', cartInfo) 
     .then(response => response)
-    .catch(err => err)
+    .catch(err => err);
 }
 
-export {sendCart}
+const addOrder = (order) => {
+  return api.post('/pedido/create', order)
+    .then(response => response)
+    .catch(err => err);
+}
+
+export { sendCart, addOrder };

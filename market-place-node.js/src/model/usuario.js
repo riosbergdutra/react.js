b9ca7c0e447fda526887f420ed3a4d6a237ac784
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-19 17:48:49 -0300

    Feat: novas funcionalidades para usuario

--- market-place-node.js/src/model/usuario.js
+++ market-place-node.js/src/model/usuario.js
@@ -1,20 +1,44 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt')
-const usuarioSchema = new mongoose.Schema({
-    nome: { type: String, unique: true, required: true },
-    email: { type: String, required: true },
+const bcrypt = require("bcrypt");
+
+const UsuarioSchema = new mongoose.Schema({
+    nome: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     senha: { type: String, required: true },
-    imagem: { type: String },
-    produtos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Produto' }],
+    imagem: { type: String, required: true },
+    enderecos: [
+        {
+            rua: { type: String, required: true},
+            numero: { type: Number, required: true},
+            complemento: { type: String, required: false},
+            CEP: { type: String, required: true},
+            createdAt: { type: Date, required: true, default: Date.now()},
+        }
+    ],
+    produtos_fav: [
+        {
+            _id: { type: mongoose.Schema.Types.ObjectId, unique: true, ref: "produtos"},
+            createdAt: { type: Date, default: Date.now()},
+        }
+    ],
+    createdAt: { type: Date, required: true, default: Date.now()},
+    admin: { type: Boolean, required: true, default: false },
 });
 
-usuarioSchema.pre("save", async function(next) {
-    if (this.senha) {
+UsuarioSchema.pre("save", async function(next) {
+    if(this.senha){
         this.senha = await bcrypt.hash(this.senha, 10);
     }
     next();
 });
 
-const Usuario = mongoose.model("Usuario", usuarioSchema);
+UsuarioSchema.pre("findOneAndUpdate", async function(next) {
+    if(this._update.senha){
+        this._update.senha = await bcrypt.hash(this._update.senha, 10);
+    }
+    next();
+})
+
+const Usuario = mongoose.model("usuarios", UsuarioSchema);
 
 module.exports = Usuario;

48d1b54a1f20f336bfe7602ac77e32ec87789de2
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-12-19 17:47:51 -0300

    correcoes para model categoria

--- market-place-node.js/src/model/categoria.js
+++ market-place-node.js/src/model/categoria.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const categoriaSchema = new mongoose.Schema({
-    nome: {
-    type:String,
-    required: true
-},
-})
+const CategoriaSchema = new mongoose.Schema({
+    nome: { type: String, unique: true, required: true},
+    createdAt: { type: Date, required: true, default: Date.now() },
+});
 
-const Categoria = mongoose.model('Categoria', categoriaSchema);
+const Categoria = mongoose.model("categorias", CategoriaSchema);
 
 module.exports = Categoria;

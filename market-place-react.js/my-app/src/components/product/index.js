0d76bc1e5ce6e9372e1dc6d7617c961fb97ed1cb
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-17 14:30:56 -0300

    retirada do span

--- market-place-react.js/my-app/src/components/product/index.js
+++ market-place-react.js/my-app/src/components/product/index.js
@@ -4,7 +4,6 @@ const Product = ({product}) => {
   const navigate = useNavigate()
   return (
     <div className='bg-white border border-gray-100 transition transform duration-700 hover:shadow-xl hover:scale-105 p-4 rounded-lg relative'>
-      <span className='bg-red-100 border border-red-500 rounded-full text-primary text-sm px-4 py-1 inline-block mb-4'>Hamburger</span>
       <img className='w-64 mx-auto transform transition duration-300 hover:scale-105' src={product.imagem} alt='imagem do produto'/>
       <div className='flex flex-col items-center my-3 space-y-2'>
         <h1 className='text-gray-900 text-lg'>{product.nome}</h1>
